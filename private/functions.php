0759224810b89a2f97060905698c7e4eeecc02ed
miguel <phatbastard81@yahoo.com>
2019-01-02 18:29:33 +0000

    headers and redirects

--- private/functions.php
+++ private/functions.php
@@ -23,4 +23,18 @@
         return htmlspecialchars($string);
     }
     
+    function error_404(){
+        header($_SERVER["SERVER_PROTOCAL"] . "404: Not Found");
+        exit();
+    }
+    
+    function error_500(){
+        header($_SERVER["SERVER_PROTOCAL"] . "500: Internal Server Error");
+        exit();
+    }
+    
+    function redirect_to($location){
+        header("Location: " . $location);
+        exit();
+    }
 ?>
